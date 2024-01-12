1455497be9d838b79eb41972019ce729f4211cdb
Selim <selimdev00@gmail.com>
2024-01-12 18:20:30 +0500

    [input]: edit mode with signals

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,8 +1,19 @@
 import styles from "./Timer.module.scss";
 
 import Button from "./Button.tsx";
+import { createSignal } from "solid-js";
 
 const Timer = () => {
+  const [editMode, setEditMode] = createSignal(true);
+
+  const [minutes, setMinutes] = createSignal();
+  const [seconds, setSeconds] = createSignal(0);
+
+  const handleMinutesChange = (e) => {
+    const value = e.target.value;
+    console.log(value);
+  };
+
   return (
     <div class={styles.timer__wrapper}>
       <div class={styles.timer__input__wrapper}>
@@ -22,10 +33,30 @@ const Timer = () => {
           <Button>Reset</Button>
         </div>
 
-        <div class={styles.timer__time}>
-          <span>09</span>
-          <span>:</span>
-          <span>54</span>
+        <div class={styles.timer__box}>
+          {editMode() ? (
+            <div class={styles.timer__time__input__wrapper}>
+              <div class={styles.timer__time__input}>
+                <input
+                  type="number"
+                  value={minutes()}
+                  onInput={handleMinutesChange}
+                />
+              </div>
+
+              <div>:</div>
+
+              <div class={styles.timer__time__input}>
+                <input type="number" />
+              </div>
+            </div>
+          ) : (
+            <div class={styles.timer__time} onClick={() => setEditMode(true)}>
+              <span>10</span>
+              <span>:</span>
+              <span>00</span>
+            </div>
+          )}
         </div>
 
         <div class={styles.timer__buttons}>
